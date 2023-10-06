c3f970b42cfc3c71cffb121c2ea05ce3e9137412
MollyEY <n11241250@qut.edu.au>
2023-10-06 10:05:21 +1000

    complete app

--- travel/models.py
+++ travel/models.py
@@ -1,7 +1,8 @@
 from . import db
 from datetime import datetime
+from flask_login import UserMixin
 
-class User(db.Model):
+class User(db.Model, UserMixin):
     __tablename__ = 'users' # good practice to specify table name
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(100), index=True, unique=True, nullable=False)
@@ -11,6 +12,10 @@ class User(db.Model):
     password_hash = db.Column(db.String(255), nullable=False)
     # relation to call user.comments and comment.created_by
     comments = db.relationship('Comment', backref='user')
+
+    # string print method
+    def __repr__(self):
+        return f"Name: {self.name}"
 
 class Destination(db.Model):
     __tablename__ = 'destinations'
@@ -22,18 +27,20 @@ class Destination(db.Model):
     # ... Create the Comments db.relationship
 	# relation to call destination.comments and comment.destination
     comments = db.relationship('Comment', backref='destination')
-	
-    def __repr__(self): #string print method
-        return f"<Name: {self.name}>"
+
+	# string print method
+    def __repr__(self):
+        return f"Name: {self.name}"
 
 class Comment(db.Model):
     __tablename__ = 'comments'
     id = db.Column(db.Integer, primary_key=True)
     text = db.Column(db.String(400))
     created_at = db.Column(db.DateTime, default=datetime.now())
-    #add the foreign key
+    # add the foreign keys
     user_id = db.Column(db.Integer, db.ForeignKey('users.id'))
     destination_id = db.Column(db.Integer, db.ForeignKey('destinations.id'))
 
+    # string print method
     def __repr__(self):
-        return f"<Comment: {self.text}>"
+        return f"Comment: {self.text}"
